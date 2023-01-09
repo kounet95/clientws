1cbd2e917c780af74e82edb921e8f1b79deb6bcc
Alpha oumar Diallo <alpha-oumar.diallo.11@ulaval.ca>
2023-01-09 21:03:08 +0100

    Initial commit

---
+++ src/service/Client.java
@@ -0,0 +1,12 @@
+package service;
+
+import javax.rmi.CORBA.Stub;
+import javax.xml.ws.WebServiceContext;
+
+public class Client {
+   public static void main(String[] args) {
+    
+       WebServiceContext stub = new BanqueServiceService().
+   }
+   
+}
---
+++ src/service/Compte.java
@@ -0,0 +1,109 @@
+
+package service;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlSchemaType;
+import javax.xml.bind.annotation.XmlType;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+
+/**
+ * <p>Classe Java pour compte complex type.
+ * 
+ * <p>Le fragment de schéma suivant indique le contenu attendu figurant dans cette classe.
+ * 
+ * <pre>
+ * &lt;complexType name="compte">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="condeCompte" type="{http://www.w3.org/2001/XMLSchema}long" minOccurs="0"/>
+ *         &lt;element name="dateCreation" type="{http://www.w3.org/2001/XMLSchema}dateTime" minOccurs="0"/>
+ *         &lt;element name="solde" type="{http://www.w3.org/2001/XMLSchema}double"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "compte", propOrder = {
+    "condeCompte",
+    "dateCreation",
+    "solde"
+})
+public class Compte {
+
+    protected Long condeCompte;
+    @XmlSchemaType(name = "dateTime")
+    protected XMLGregorianCalendar dateCreation;
+    protected double solde;
+
+    /**
+     * Obtient la valeur de la propriété condeCompte.
+     * 
+     * @return
+     *     possible object is
+     *     {@link Long }
+     *     
+     */
+    public Long getCondeCompte() {
+        return condeCompte;
+    }
+
+    /**
+     * Définit la valeur de la propriété condeCompte.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link Long }
+     *     
+     */
+    public void setCondeCompte(Long value) {
+        this.condeCompte = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété dateCreation.
+     * 
+     * @return
+     *     possible object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public XMLGregorianCalendar getDateCreation() {
+        return dateCreation;
+    }
+
+    /**
+     * Définit la valeur de la propriété dateCreation.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link XMLGregorianCalendar }
+     *     
+     */
+    public void setDateCreation(XMLGregorianCalendar value) {
+        this.dateCreation = value;
+    }
+
+    /**
+     * Obtient la valeur de la propriété solde.
+     * 
+     */
+    public double getSolde() {
+        return solde;
+    }
+
+    /**
+     * Définit la valeur de la propriété solde.
+     * 
+     */
+    public void setSolde(double value) {
+        this.solde = value;
+    }
+
+}
